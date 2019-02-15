8f719c925b22345998a3a0f9700a6d1b8296654e
Rodolfo Bermúdez-Neubauer <intellego-rbn@users.noreply.github.com>
2019-02-15 04:58:33 -0300

    l10n_cl
    Módulo de Tesorería y Pagos

---
+++ l10n_cl_pagos/README.rst
@@ -0,0 +1,33 @@
+# Odoo Chile - Tesorería y Pagos Salientes
+[l10n_cl_pagos]
+
+## Odoo (ERP/CRM)
+ - Odoo 12 Localización chilena
+ - Módulos ajustados para versión 12 (Enterprise)
+
+## Scope 
+**Features list: Chilean Localization**
+
+    * Órdenes de Pago saliente
+    * Pagos masivos para proveedores y empleados
+    * Generación de archivo de nómina de pago para bancos (Chile y BCI)
+    * Medios y vías de Pagos para Chile<br/>Total a Cobrar y a Pagar en Maestro de Terceros
+
+## Credits
+<p>
+<img width="200" alt="Logo Intellego-BI" src="https://i2.wp.com/intellego-bi.com/ws/wp-content/uploads/2016/05/Intellego-BI-112x35.jpg" />
+</p>
+
+**Intellego-BI.com** - https://intellego-bi.com
+ - Rodolfo Bermúdez Neubauer <odoo@intellego-bi.com>
+
+
+ ## To do
+ 
+ - Translations
+ 
+ 
+ ## Disclaimer
+ 
+ - Open source software: use at your own risk. 
+ - Public private repository. Not currently supporting third parties
---
+++ l10n_cl_pagos/__manifest__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+###################################################################################
+#
+#    Intellego-BI.com
+#    Copyright (C) 2017-TODAY Intellego Business Intelligence S.A.(<http://www.intellego-bi.com>).
+#    Author: Rodolfo Bermúdez Neubauer(<https://www.intellego-bi.com>)
+#
+#    This program is free software: you can modify
+#    it under the terms of the GNU Affero General Public License (AGPL) as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# 
+###################################################################################
+{
+    'name': 'Chile - Tesorería y Pagos',
+    'summary': """Menú de Tesorería para gestión de pagos y contabilidad bancaria""",
+    'version': '12.0.1.0.0',
+    'description': """Órdenes de Pago Masivo a Proveedores y medios de pago""",
+    'author': 'Intellego-BI.com',
+    'company': 'Intellego-BI.com',
+    'website': 'https://www.Intellego-BI.com',
+    'maintainer': 'Intellego-BI.com',
+    'category': 'Accounting',
+    'depends': ['base', 'account_accountant', 'purchase'],
+    'license': 'LGPL-3',
+    'data': [
+        'views/cl_treasury_menu.xml',
+    ],
+    'demo': [],
+    'images': ['static/description/banner.jpg'],
+    'installable': True,
+    'auto_install': False,
+}
