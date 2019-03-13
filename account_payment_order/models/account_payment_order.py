7590bd760a45263668b27ddddb161799fa0dbdb7
Rodolfo Bermúdez-Neubauer <intellego-rbn@users.noreply.github.com>
2019-03-13 18:57:55 -0300

    Update account_payment_order.py

--- account_payment_order/models/account_payment_order.py
+++ account_payment_order/models/account_payment_order.py
@@ -322,7 +322,7 @@ class AccountPaymentOrder(models.Model):
         #        "installed the related Odoo module."))
             for payline in self.payment_line_ids:
 #                payment_file_content = payline.name + ',' + payline.partner_id.name + ',' + str(payline.partner_id.email) + ',' + payline.partner_id.document_number + ',' + payline.communication + ',' + str(payline.amount_company_currency) + ',' + str(payline.partner_bank_id.bank_id.name) + ',' + str(payline.partner_bank_id.acc_number) + ',' + str(payline.ml_maturity_date) + ',' + str(payline.date) + ',' + '\n'
-                f_bank_code = payline.partner_bank_id.bank_id.bic[5:3]
+                f_bank_code = payline.partner_bank_id.bank_id.bic
                 f_rut = ""
                 f_rut_dv = ""
                 f_rut, f_rut_dv = payline.partner_id.document_number.split("-")
