ea1aefb66c2528adfc689e9e91fc1104f9b6aba7
Rodolfo Bermúdez-Neubauer <intellego-rbn@users.noreply.github.com>
2019-03-20 17:16:30 -0300

    Update employee_creation_from_user.py

--- l10n_cl_hr_empleado_usuario/models/employee_creation_from_user.py
+++ l10n_cl_hr_empleado_usuario/models/employee_creation_from_user.py
@@ -81,11 +81,15 @@ class ResUsersInherit(models.Model):
                               ('id', '=', check_id)])
             if len(partner_ids) == 1:
                 for partner in partner_ids:
+                    #raise ValidationError(_(
+                    #    "UserID %s / Type = %s / UserPartnerID %s / PartnerID %s / PartEmpl %s")
+                    #    % (self.id, self.user_type, user.partner_id.id, ))
+
                     if self.user_type == 'empl':
-                        partner.write({'employee': 'true'})
+                        self.partner_id.write({'employee': 'true'})
                         return
                     else:
-                        partner.write({'employee': 'false'})
+                        self.partner_id.write({'employee': 'false'})
                         return
             else:
                 raise UserError(_('Se han encontrado %s partners') % (len(partner_ids)))
