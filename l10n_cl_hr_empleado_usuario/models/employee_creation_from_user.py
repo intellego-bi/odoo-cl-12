b0b9e52f78acfa477b15300f403f644f2d3df33e
Rodolfo Bermúdez-Neubauer <intellego-rbn@users.noreply.github.com>
2019-03-20 17:37:34 -0300

    Update employee_creation_from_user.py

--- l10n_cl_hr_empleado_usuario/models/employee_creation_from_user.py
+++ l10n_cl_hr_empleado_usuario/models/employee_creation_from_user.py
@@ -88,10 +88,10 @@ class ResUsersInherit(models.Model):
                     #    % (self.id, self.user_type, user.partner_id.id, ))
 
                     if self.user_type == 'empl':
-                        self.partner_id.write({'employee': bool_emp, 'ref': self.identification_id})
+                        self.partner_id.write({'employee': bool_emp, 'user_id': self.id, 'ref': self.identification_id})
                         return
                     else:
-                        self.partner_id.write({'employee': bool_int, 'ref': self.identification_id})
+                        self.partner_id.write({'employee': bool_int, 'user_id': self.id, 'ref': self.identification_id})
                         return
             else:
                 raise UserError(_('Se han encontrado %s partners') % (len(partner_ids)))
